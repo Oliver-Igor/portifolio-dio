115f704c8352f8149c36c6da6d3bcfebbcda88e5
Oliver-Igor <valterigor.florestal@gmail.com>
2024-01-17 23:22:03 -0300

    first commit

---
+++ src/screens/MainScreen/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+
+
+export function MainScreen() {
+  return (
+    <View style={{flex: 1}}>
+
+    </View>
+  );
+}
---
+++ src/screens/SkillScreen/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+
+
+export function SkillScreen() {
+  return (
+    <View style={{flex: 1}}>
+
+    </View>
+  );
+}
